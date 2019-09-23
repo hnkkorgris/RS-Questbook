69c0b81ca0e5b2ec9fd5d3fbbc33ac9ceacd6873
hnkkorgris <vjsigler@ncsu.edu>
2019-09-23 15:10:41 -0400

    Added animation states between idle and walkLeft.

---
+++ RS Questbook/Assets/Behaviors/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D _rBody;
+    private Animator _animator;
+    private float _maxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rBody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _maxSpeed = 8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate requested movement
+        var direction = GetDirectionFromInputs();
+        var velocity = GetVelocity(direction);
+        Debug.Log($"direction.x: {direction.x}");
+
+        // Update player position
+        _rBody.velocity = velocity;
+
+        // Update sprite animation
+        _animator.SetInteger("xMoveDirection", direction.x);
+    }
+
+    private Vector2Int GetDirectionFromInputs()
+    {
+        var direction = new Vector2Int(0, 0);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction.y++;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction.y--;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction.x--;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction.x++;
+        }
+
+        return direction;
+    }
+
+    private Vector2 GetVelocity(Vector2Int direction)
+    {
+        var velocity = new Vector2(0f, 0f);
+
+        velocity.x += direction.x * _maxSpeed;
+        velocity.y += direction.y * _maxSpeed;
+
+        return velocity;
+    }
+}
