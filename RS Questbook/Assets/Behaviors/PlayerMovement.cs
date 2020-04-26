a94b42e5f8e03219d4c1932a99a8aa3fc1afe7c4
hnkkorgris <vjsigler@ncsu.edu>
2020-04-26 17:42:31 -0400

    Added a simple box collider and implemented wall collision detection.

--- RS Questbook/Assets/Behaviors/PlayerMovement.cs
+++ RS Questbook/Assets/Behaviors/PlayerMovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rBody;
+    private BoxCollider2D _collider;
     private Animator _animator;
+
     private float _maxSpeed;
+    private Vector2 _counterVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
         _rBody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<BoxCollider2D>();
         _animator = GetComponent<Animator>();
+
         _maxSpeed = 8f;
+        _counterVelocity = new Vector2(0f, 0f);
     }
 
     // Update is called once per frame
@@ -21,7 +27,6 @@ public class PlayerMovement : MonoBehaviour
         // Calculate requested movement
         var direction = GetDirectionFromInputs();
         var velocity = GetVelocity(direction);
-        Debug.Log($"direction.x: {direction.x}");
 
         // Update player position
         _rBody.velocity = velocity;
@@ -29,6 +34,16 @@ public class PlayerMovement : MonoBehaviour
         // Update sprite animation
         _animator.SetInteger("xMoveDirection", direction.x);
         _animator.SetInteger("yMoveDirection", direction.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _counterVelocity = collision.relativeVelocity;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        _counterVelocity = new Vector2(0f, 0f);
     }
 
     private Vector2Int GetDirectionFromInputs()
@@ -59,8 +74,8 @@ public class PlayerMovement : MonoBehaviour
     {
         var velocity = new Vector2(0f, 0f);
 
-        velocity.x += direction.x * _maxSpeed;
-        velocity.y += direction.y * _maxSpeed;
+        velocity.x += direction.x * _maxSpeed + _counterVelocity.x;
+        velocity.y += direction.y * _maxSpeed + _counterVelocity.y;
 
         return velocity;
     }
